4a24dfd17111fba37ec60998ead8b3229e5002c3
Velasco_182 <ruben.velasco0@misena.edu.co>
2024-05-17 17:50:18 -0500

    Prueba 50%, sin crear

--- Proyectos/pruebaPhp/app.js
+++ Proyectos/pruebaPhp/app.js
@@ -1,6 +1,8 @@
+///FUNCIÓN PARA HACER GET A LOS DATOS DE LA DB///
+let ruta = '/sena/Proyectos/pruebaPhp/back.php';
 const fetchData = () =>{
     //Ruta para ejecución del Fetch en este caso el back.php
-    fetch('/sena/Proyectos/pruebaPhp/back.php')
+    fetch(ruta)
     .then(response => response.json())
     .catch(err => console.error('Error al obtener datos: ', err))
     .then( data => {
@@ -8,9 +10,50 @@ const fetchData = () =>{
         //document.getElementById('dataContainer').innerText = JSON.stringify(data);
     });
 }
-
+////HACEMOS UN EVENTO DE ESCUCHA EN EL BOTON DE OBTENER
 document.getElementById('fetchData').addEventListener('click', fetchData)
+///FUNCIÓN PARA CREAR DATOS HACIENDO UN POST
+function fetchCrear(){
 
+    $('#miFormulario').submit(function(event) {
+        event.preventDefault();
+
+        console.log("en formulario");
+      
+        // Recopilar los datos del formulario
+       /*var nombre = document.getElementById('nombreCliente').value;
+        var apellido = document.getElementById('apellidoCliente').value;
+        var telefono = document.getElementById('telefonoCliente').value;
+        */
+        let formulario = new FormData(document.getElementById("miFormulario"))
+
+        // Enviar los datos al servidor utilizando fetch
+        fetch(ruta, {
+          method: 'POST',
+          body: formulario
+          /*headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono
+          })*/
+        })
+       .then(response => response.json())
+       .catch(error => console.error(error))
+       .then(data => {
+          console.log(data);
+      
+          // Cerrar el modal
+          $('#exampleModal').modal('hide');
+        });
+       
+      });
+}
+////HACEMOS UN EVENTO DE ESCUCHA EN EL BOTON DE CREAR*/
+document.getElementById('btn-asmet').addEventListener('click', fetchCrear)
+////RENDERIZAMOS LOS DATOS OBTENIDOS EN UNA TABLA
 function renderTable(data) {
     const tbody = document.querySelector('#myTable tbody');
     
@@ -29,5 +72,4 @@ function renderTable(data) {
         tbody.appendChild(row);
         
     });
-
 }
--- Proyectos/pruebaPhp/back.php
+++ Proyectos/pruebaPhp/back.php
@@ -14,7 +14,7 @@
     }
     
     //echo "Conectado a la base de datos";
-    
+    ###ABRO READ###
     // Consulta para obtener datos (ejemplo) 
     $sql = "SELECT * FROM prueba.clientes"; 
     $result = $connection->query($sql); 
@@ -37,6 +37,34 @@
     
     // Devolver los datos como JSON 
     echo json_encode($data); 
+
+    ###CIERRO READ###
+    ###ABRO CREATE###
+    // Procesar la petición
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Obtener los datos enviados en el cuerpo de la petición
+        //$data = json_decode(file_get_contents('php://input'), true);
+        $dato = $_POST['formulario'];
+    /*$nombre = $_POST['nombreCliente'];
+    $apellido = $_POST['apellidoCliente'];
+    $telefono = $_POST['telefonoCliente'];*/
+        // Insertar los datos en la base de datos
+        $insertar = "INSERT INTO prueba.clientes (nombre, apellido, email) VALUES ($dato)";
+        /*$stmt = $connection->prepare($sql);
+        $stmt->bind_param("sss", $data['nombre'], $data['apellido'], $data['telefono']);
+        $stmt->execute();*/
+    //$result = $connection->query($insertar); 
+        // Devolver un mensaje de éxito
+        mysqli_query($con, $insertar);
+        
+        echo json_encode(array('message' => 'Registro creado exitosamente '.$dato));
+    } else {
+        // Devolver un mensaje de error si no se envió una petición POST
+        echo json_encode(array('error' => 'No se envió una petición POST'));
+    }
+
+    ###CIERRO CREATE###
+
     
     // Cerrar la conexión 
     $connection->close(); 
