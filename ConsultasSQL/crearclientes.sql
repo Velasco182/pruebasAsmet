05405578d575a75ad6d399792191c6d2999fb70f
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-05-23 17:01:04 -0500

    Update crearclientes.sql

--- ConsultasSQL/crearclientes.sql
+++ ConsultasSQL/crearclientes.sql
@@ -1,3 +1,3 @@
 USE prueba;
 
-CREATE TABLE clientes(id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(10), apellido VARCHAR(10), telefono INTEGER(10)); 
+CREATE TABLE clientes(id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(10), apellido VARCHAR(10), telefono BIGINT(10)); 
