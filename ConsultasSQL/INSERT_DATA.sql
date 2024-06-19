82c5f00008f53530874d8db4662c1db3e9dd8c9d
Velasco182 <ruben.velasco0@misena.edu.co>
2024-06-19 17:14:52 -0500

    SQL del día

---
+++ ConsultasSQL/INSERT_DATA.sql
@@ -0,0 +1,9 @@
+/*Insertar datos en la tabla BIG_TIPO_COMPENSATORIO*/
+INSERT INTO "CONSULTA_PBI"."BIG_TIPO_COMPENSATORIO"
+("TIP_COM_NOMBRE", "TIP_COM_DESCRIPCION")
+VALUES
+('Vacaciones no recibidas', 'Compensación por vacaciones no disfrutadas.'),
+('Reporte de NURC', 'Compensación por informe de nurc.'),
+('Festivos trabajados', 'Compensación por días festivos trabajados.'),
+('Horas extra trabajadas', 'Compensación por horas extra trabajadas.');
+/*---------------------------------------------------------------------------*/
